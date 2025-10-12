1ec61e4be3545a6f9c26c8be97191ff4b468a086
Driftedbucket <ndinowix@gmail.com>
2025-10-12 22:08:18 +0200

    Added a php script(i think thats what is called) to to verify a user exists in our database and give them access(user landing page not yet implemented)

--- login.php
+++ login.php
@@ -16,3 +16,36 @@
         <p>Don’t have an account? <a href="register.php">Register here</a></p>
     </form>
 </div>
+
+<?php
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['role'] = $user['role'];
+            $_SESSION['name'] = $user['name'];
+
+            echo "<script>alert('Welcome back, " . $user['name'] . "!'); window.location='index.php';</script>";
+        } else {
+            echo "<script>alert('Incorrect password');</script>";
+        }
+    } else {
+        echo "<script>alert('User not found');</script>";
+    }
+
+    $stmt->close();
+}
+$conn->close();
+?>
+
+<?php include('includes/footer.php'); ?>
