4ab06ba0025d0b32bbc63b84506f2464d6760d32
Awike Gulu <ndinowix@gmail.com>
2025-10-15 15:17:18 +0200

    Update database connection parameters

--- includes/connection.php
+++ includes/connection.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
+$port = 3306;
 $user = "root";
-$pass = "muddysituation";
+$pass = ""; // Empty password - default for XAMPP
 $dbname = "community_job_board";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
