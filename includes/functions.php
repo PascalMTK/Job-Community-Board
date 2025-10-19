6a91cd3c64c038d72b98fa3ab34a1b9c7575f957
Awike Gulu <ndinowix@gmail.com>
2025-10-19 03:06:01 +0200

    Refactor functions and add user ID and salary formatting
    Added get_user_id and format_salary functions to handle user ID retrieval and salary formatting.

--- includes/functions.php
+++ includes/functions.php
@@ -16,3 +16,17 @@ function is_employer() {
 function is_student() {
     return isset($_SESSION['role']) && $_SESSION['role'] === 'student';
 }
+
+function get_user_id() {
+    return isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+}
+
+// Format salary range
+function format_salary($min, $max, $currency = 'N$') {
+    if ($min && $max) {
+        return $currency . number_format($min, 0) . ' - ' . $currency . number_format($max, 0);
+    } elseif ($min) {
+        return $currency . number_format($min, 0) . '+';
+    }
+    return 'Negotiable';
+}
