f69a2326fec6eb864a24dba68bf9ba5912fe31f8
Awike Gulu <ndinowix@gmail.com>
2025-10-19 03:06:53 +0200

    Add time_ago function to calculate elapsed time
    Added a new function to calculate time ago from a given datetime.

--- includes/functions.php
+++ includes/functions.php
@@ -30,3 +30,30 @@ function format_salary($min, $max, $currency = 'N$') {
     }
     return 'Negotiable';
 }
+
+// Time ago function
+function time_ago($datetime) {
+    $timestamp = strtotime($datetime);
+    $diff = time() - $timestamp;
+    
+    if ($diff < 60) {
+        return $diff . ' seconds ago';
+    } elseif ($diff < 3600) {
+        $minutes = floor($diff / 60);
+        return $minutes . ' minute' . ($minutes > 1 ? 's' : '') . ' ago';
+    } elseif ($diff < 86400) {
+        $hours = floor($diff / 3600);
+        return $hours . ' hour' . ($hours > 1 ? 's' : '') . ' ago';
+    } elseif ($diff < 604800) {
+        $days = floor($diff / 86400);
+        return $days . ' day' . ($days > 1 ? 's' : '') . ' ago';
+    } elseif ($diff < 2592000) {
+        $weeks = floor($diff / 604800);
+        return $weeks . ' week' . ($weeks > 1 ? 's' : '') . ' ago';
+    } else {
+        return date('M d, Y', $timestamp);
+    }
+}
+
+
+
